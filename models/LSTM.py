6f01aaa131611515c6106cc16274a44743d148ba
MaroonBlue <martyna.majchrzak19@gmail.com>
2023-04-23 15:39:58 +0200

    LSTM new architectures

--- models/LSTM.py
+++ models/LSTM.py
@@ -2,21 +2,44 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import LSTM, Dropout, Dense, Activation, BatchNormalization
 from sklearn.preprocessing import OneHotEncoder
 
-def LSTM(X_t, y_t, dropout):
+def LSTM1(input_shape, output_nodes, dropout):
     model = Sequential()
     model.add(BatchNormalization())
-    model.add(LSTM(
-        256,
-        input_shape=X_t.shape[1:],
-        return_sequences=True
-    ))
+    model.add(LSTM(256,input_shape=input_shape,return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(256, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(Dense(output_nodes))
+    model.add(Activation('softmax'))
+    return model
+
+def LSTM2(input_shape, output_nodes, dropout):
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(LSTM(256, input_shape=input_shape,return_sequences=True))
     model.add(Dropout(dropout))
     model.add(LSTM(512, return_sequences=True))
     model.add(Dropout(dropout))
     model.add(LSTM(256))
     model.add(Dense(256))
     model.add(Dropout(dropout))
-    model.add(Dense(y_t.shape[1]))
+    model.add(Dense(output_nodes))
     model.add(Activation('softmax'))
+    return model
 
+def LSTM3(input_shape, output_nodes, dropout):
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(LSTM(512, input_shape=input_shape,return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(LSTM(512))
+    model.add(Dropout(dropout))
+    model.add(LSTM(256))
+    model.add(Dropout(dropout))
+    model.add(Dense(256))
+    model.add(Dropout(dropout))
+    model.add(Dense(output_nodes))
+    model.add(Activation('softmax'))
     return model
