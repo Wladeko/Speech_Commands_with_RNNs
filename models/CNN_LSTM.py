d6a17a5a33cb818824e33af179c2e5ca15062b31
MaroonBlue <martyna.majchrzak19@gmail.com>
2023-04-23 16:25:11 +0200

    CNN_LSTM architecture update

--- models/CNN_LSTM.py
+++ models/CNN_LSTM.py
@@ -1,67 +1,31 @@
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras import layers as L
-from tensorflow.keras import backend as K
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras import optimizers
-
-from sklearn.metrics import confusion_matrix
 from tensorflow.keras.models import Sequential
-from kapre.time_frequency import Melspectrogram, Spectrogram
+from tensorflow.keras.layers import LSTM, Dropout, Dense, Activation, BatchNormalization, Permute, Conv2D, MaxPooling2D
 from kapre.utils import Normalization2D
 
-def CNN_LSTM(X_t, y_t, dropout):
-    # CNN part
-    input = L.Input(X_train.shape[1:], name='input')
-    x = L.Reshape((1, -1))(input)
+def CNN_LSTM1(input_shape, output_nodes, dropout):
+    ## CNN
+    model = Sequential()
+    model.add(Normalization2D())
+    model.add(Conv2D(20, (5, 1), input_shape=input_shape, activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 1)))
+    model.add(Dropout(dropout))
+    model.add(Conv2D(40, (3, 3), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(dropout))
+    model.add(Conv2D(1, (5, 1), activation='relu', padding='same'))
+    model.add(BatchNormalization())
+    
+    ## LSTM
+    model.add(Lambda(lambda x: K.squeeze(x, -1), name='sqeeze_last_dim'))
+    model.add(LSTM(64, return_sequences=True))
+    model.add(LSTM(64, return_sequences=True))
+    model.add(Lambda(lambda q: q[:, -1]))
 
-    m = Melspectrogram(n_dft=1024, n_hop=128, input_shape=(1, None),
-                    padding='same', sr=16000, n_mels=80,
-                    fmin=40.0, fmax=16000 / 2, power_melgram=1.0,
-                    return_decibel_melgram=True, trainable_fb=False,
-                    trainable_kernel=False,
-                    name='mel_stft')
-    m.trainable = False
-    x = m(x)
-
-    x = Normalization2D(int_axis=0, name='mel_stft_norm')(x)
-
-    x = L.Permute((2, 1, 3))(x)
-
-    x = L.Conv2D(20, (5, 1), activation='relu', padding='same')(x)
-    x = L.BatchNormalization()(x)
-    x = L.MaxPooling2D((2, 1))(x)
-    x = L.Dropout(dropout)(x)
-
-    x = L.Conv2D(40, (3, 3), activation='relu', padding='same')(x)
-    x = L.BatchNormalization()(x)
-    x = L.MaxPooling2D((2, 2))(x)
-    x = L.Dropout(dropout)(x)
-
-    x = L.Conv2D(1, (5, 1), activation='relu', padding='same')(x)
-    x = L.BatchNormalization()(x)
-
-    # LSTM part
-    x = L.Lambda(lambda x: K.squeeze(x, -1), name='sqeeze_last_dim')(x)
-    x = L.Bidirectional(L.LSTM(64, return_sequences=True))(x)
-    x = L.Bidirectional(L.LSTM(64, return_sequences=True))(x)  # it returns [b_s, seq_len, vec_dim]
-
-    xFirst = L.Lambda(lambda q: q[:, -1])(x)  # to [b_s, vec_dim]
-    query = L.Dense(128)(xFirst)
-
-    # Attention Part
-    attScores = L.Dot(axes=[1, 2])([query, x])
-    attScores = L.Softmax(name='attSoftmax')(attScores)
-
-    # Rescaling
-    attVector = L.Dot(axes=[1, 1])([attScores, x])
-
-    x = L.Dense(64, activation='relu')(attVector)
-    x = L.Dense(32)(x)
-
-    output = L.Dense(31, activation='softmax', name='output')(x)
-
-    model = Model(inputs=[input], outputs=[output])
+    ## Dense
+    model.add(Dense(128))
+    model.add(Dense(32))
+    model.add(Dense(output_nodes, activation='softmax', name='output'))
 
     return model
