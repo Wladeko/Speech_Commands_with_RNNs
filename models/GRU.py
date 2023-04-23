d0652946cb47f6fc991f9bf1bca81c4648bc7461
MaroonBlue <martyna.majchrzak19@gmail.com>
2023-04-23 15:50:44 +0200

    GRU new architectures

--- models/GRU.py
+++ models/GRU.py
@@ -1,22 +1,45 @@
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import SimpleRNN, Dropout, Dense, Activation, BatchNormalization
+from tensorflow.keras.layers import Dropout, Dense, Activation, BatchNormalization
 from sklearn.preprocessing import OneHotEncoder
 
-def GRU(X_t, y_t, dropout):
+def GRU1(input_shape, output_nodes, dropout):
     model = Sequential()
     model.add(BatchNormalization())
-    model.add(GRU(
-        128,
-        input_shape=X.shape[1:],
-        return_sequences=True
-    ))
+    model.add(GRU(128, input_shape=input_shape, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(GRU(128, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(Dense(output_nodes))
+    model.add(Activation('softmax'))
+    return model
+
+
+def GRU2(input_shape, output_nodes, dropout):
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(GRU(256, input_shape=input_shape, return_sequences=True))
     model.add(Dropout(dropout))
     model.add(GRU(128, return_sequences=True))
     model.add(Dropout(dropout))
     model.add(GRU(128))
     model.add(Dense(128))
     model.add(Dropout(dropout))
-    model.add(Dense(y.shape[1]))
+    model.add(Dense(output_nodes))
     model.add(Activation('softmax'))
+    return model
 
+def GRU3(input_shape, output_nodes, dropout):
+    model = Sequential()
+    model.add(BatchNormalization())
+    model.add(GRU(512, input_shape=input_shape, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(GRU(256, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(GRU(256, return_sequences=True))
+    model.add(Dropout(dropout))
+    model.add(GRU(256))
+    model.add(Dense(256))
+    model.add(Dropout(dropout))
+    model.add(Dense(output_nodes))
+    model.add(Activation('softmax'))
     return model
